32e4dc161bfc690f604448105b81852cc65140b8
root <root@euan.zepko.local>
2017-02-17 16:58:55 +0000

    Admin delete users, decline solutions

--- BuildPythonPleaseGUI/views.py
+++ BuildPythonPleaseGUI/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from decorator import view_new_notification
+#from decorator import view_new_notification
 from django.contrib.auth.models import User
 from forms import UserForm, CreateProjectForm, FilterForm, SolutionForm
 from models import Projects, Solution
+
+### CUSTOM FUNCTIONS
+import uuid
+def genid(prefix):
+	return prefix+str(uuid.uuid4())
 
 def is_a_num(s):
 	try:
@@ -13,6 +18,23 @@ def is_a_num(s):
 	except ValueError:
 		return False
 
+def notifications(request):
+	user = request.user.username
+	notifications = {}
+	notifications["ids"] = []
+	for sol in Solution.objects.filter(owner=user):
+		if sol.is_new:
+			notifications["ids"].append({"proj":sol.project_id,"sol":sol.id})
+	if notifications["ids"] != []:
+		notifications["is_new"] = True
+	else:
+		notifications["is_new"] = False
+	notifications["number"] = str(len(notifications["ids"]))
+	return notifications
+
+
+
+### MAPPED FUNCTIONS
 def register(request):
 
 	error_msg = False
@@ -62,21 +84,21 @@ def success(request):
 
 def terms_and_conditions(request):
 
-	return render(request, 'BuildPythonPleaseGUI/terms_and_conditions.html')
+	return render(request, 'BuildPythonPleaseGUI/terms_and_conditions.html', {'notifications':notifications(request)})
 
 #@login_required(login_url="login")
 def home(request):
 
 	return render(request,"BuildPythonPleaseGUI/home.html", {
-			#"all_open_incidents":all_open_dict,
+			'notifications':notifications(request),
 			})
 
-@view_new_notification
+
 def about_us(request):
 	#print UserProxy
-	print request
-
-	return render(request, 'BuildPythonPleaseGUI/about_us.html')
+	#print request
+	return render(request, 'BuildPythonPleaseGUI/about_us.html', {'notifications':notifications(request)})
+	#return render(request, 'BuildPythonPleaseGUI/about_us.html')
 
 @login_required(login_url="login")
 def projects(request):
@@ -108,7 +130,8 @@ def projects(request):
 	return render(request, 'BuildPythonPleaseGUI/projects.html', {
 			"search_title":search_title,
 			"projects":projects,
-			"form":form
+			"form":form, 
+			'notifications':notifications(request)
 		})
 
 @login_required(login_url="login")
@@ -167,7 +190,8 @@ def create_project(request):
 
 		return render(request, 'BuildPythonPleaseGUI/create_project.html', {
 				"form":form,
-				"form_error":False,
+				"form_error":False, 
+				'notifications':notifications(request),
 			})
 
 @login_required(login_url="login")
@@ -199,7 +223,7 @@ def project(request, id):
 	elif request.GET.get('accept_solution') == "Accept Solution":
 		if project.status == "Open":
 			solution_id = request.GET.get('solution_id')
-			sol = Solution.objects.filter(id=solution_id)[0]
+			sol = Solution.objects.get(id=solution_id)
 			#update senders balance
 			user = User.objects.get(username=sol.sender)
 			new_balance = str(int(user.userprofile.balance) + int(project.payout))
@@ -209,7 +233,15 @@ def project(request, id):
 			#closed project
 			project.status = "Closed"
 			project.save()
-		return HttpResponseRedirect("/projects/")
+
+			for sol_in_proj in Solution.objects.filter(project_id=sol.project_id):
+				sol_in_proj.delete()
+			return HttpResponseRedirect("/projects/")
+
+	elif request.GET.get('decline') == "Decline":
+		solution_id = request.GET.get('solution_id')
+		Solution.objects.get(id=solution_id).delete()
+		return HttpResponseRedirect("/project/"+id+"/")
 
 	if is_owner:
 		solutions = []
@@ -223,6 +255,7 @@ def project(request, id):
 										"is_owner":is_owner,
 										"is_closed":is_closed,
 										"solutions":solutions,
+										'notifications':notifications(request),
 										})
 
 	if request.method == 'POST':
@@ -234,6 +267,7 @@ def project(request, id):
 		if sf.is_valid():
 
 			solution = sf.save(commit=False)
+			solution.id = genid("Solution:")
 			solution.owner = project.owner
 			solution.sender = user.username
 			solution.project_id = id
@@ -254,14 +288,19 @@ def project(request, id):
 def all_users(request):
 	#warning can change request.user
 	#look how to use session id
-	#same issue in projects, create_project and project
-	#user = User.objects.filter(username=request.user.username)
+	#same issue in projects, create_project and project, notifications
+	user = User.objects.get(username=request.user.username)
 	#print user
 	#is_admin = user.userprofile.is_admin
+	if user.userprofile.is_admin:
+		every_user = User.objects.all()
+		
+		if request.GET.get('delete_user') == "Delete User":
+			user =  request.GET.get("username")
+			User.objects.get(username=user).delete()
 
-	every_user = User.objects.all()
-
-	return render(request, 'BuildPythonPleaseGUI/all_users.html', {
-												"every_user":every_user,
-												#"is_admin":is_admin,
-												})
+		return render(request, 'BuildPythonPleaseGUI/all_users.html', {
+													"every_user":every_user,
+													'notifications':notifications(request),
+													})
+	return render(request, "BuildPythonPleaseGUI/all_users.html")
