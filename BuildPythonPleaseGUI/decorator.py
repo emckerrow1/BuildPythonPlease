9e2a941adff0bd90c9d54a66e2cce7f0a0baead0
emckerrow1 <euan.mckerrow@zepko.com>
2017-02-18 12:12:27 +0000

    Delete decorator.py

--- BuildPythonPleaseGUI/decorator.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-#test
-def view_new_notification(view_func):
-	def wrapper(request, *args, **kwargs):
-		r = view_func(request, *args, **kwargs)
-		r.context_data = {'foo': 'bar'}
-		return r.render()
-	return wrapper
